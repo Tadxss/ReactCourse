62376899c051eaf1dfaf490b3280e2a6433e9389
Daryl John Tadeo <daryltadeo@Daryls-MacBook-Pro.local>
2025-02-04 16:07:10 +0800

    Adding Score View Function

--- AlmostFinalCountdown/src/components/ResultModal.jsx
+++ AlmostFinalCountdown/src/components/ResultModal.jsx
@@ -5,6 +5,8 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
 
     const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
     useImperativeHandle(ref, () => {
         return {
             open() {
@@ -13,8 +15,9 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
         }
     });
     
-    return <dialog ref={dialog} className="result-modal">
+    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>You lost</h2>}
+        {!userLost && <h2>You Score: {score}</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
         <form method="dialog" onSubmit={onReset}>
