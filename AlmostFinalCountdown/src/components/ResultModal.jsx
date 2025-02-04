432ed9ad3c36a699846951f29803d815dacaa3b8
Daryl John Tadeo <daryltadeo@Daryls-MacBook-Pro.local>
2025-02-04 16:19:28 +0800

    Using Portals and Introduction to it to transfer block of codes to specific part of DOM

--- AlmostFinalCountdown/src/components/ResultModal.jsx
+++ AlmostFinalCountdown/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from "react"
+import { createPortal } from 'react-dom';
 
 export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
     const dialog = useRef();
@@ -15,13 +16,16 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
         }
     });
     
-    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
-        {userLost && <h2>You lost</h2>}
-        {!userLost && <h2>You Score: {score}</h2>}
-        <p>The target time was <strong>{targetTime} seconds.</strong></p>
-        <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
-        <form method="dialog" onSubmit={onReset}>
-            <button>Close</button>
-        </form>
-    </dialog>
+    return createPortal(
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>You Score: {score}</h2>}
+            <p>The target time was <strong>{targetTime} seconds.</strong></p>
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
+                <button>Close</button>
+            </form>
+        </dialog>,
+        document.getElementById('modal'),
+    );
 }
