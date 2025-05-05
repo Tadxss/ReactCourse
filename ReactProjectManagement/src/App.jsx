3ff2a5bd1dda463eebc976652078183781161697
Tadxss <tadxss@Daryl-Macbook-Pro-M4-Space-Black.local>
2025-05-05 20:08:42 +0800

    Managing / Adding Tasks & Understanding Drilling Props

--- ReactProjectManagement/src/App.jsx
+++ ReactProjectManagement/src/App.jsx
@@ -8,7 +8,26 @@ function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
+    tasks: []
   });
+
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId, 
+      };
+
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  function handleDeleteTask() {}
 
   function handleSelectProject(id) {
     setProjectsState(prevState => {
@@ -68,7 +87,15 @@ function App() {
     project => project.id === projectsState.selectedProjectId
   );
   
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
+  let content = (
+    <SelectedProject 
+      project={selectedProject} 
+      onDelete={handleDeleteProject} 
+      onAddTask={handleAddTask}
+      onDeleteTasks={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+  );
 
   if(projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
